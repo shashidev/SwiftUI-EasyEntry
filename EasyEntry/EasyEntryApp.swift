96841ebc8871f899206e84ed11d74a10ebcdbae6
Shashi Ranjan <shashi.ranjan@jublfood.com>
2024-10-19 09:39:04 +0530

    Initial Commit

---
+++ EasyEntry/EasyEntryApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyEntryApp.swift
+//  EasyEntry
+//
+//  Created by Shashi Ranjan on 19/10/24.
+//
+
+import SwiftUI
+
+@main
+struct EasyEntryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
